348e33afb3c5d4f9d662a85d56b85d6a2e794353
Om Prakash Pandey <pandeyomu16.gol@gmail.com>
2020-11-30 13:36:02 +0530

    add app base component & setup backend

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Base from "./Base";
+import { BASE_API } from "../backend";
+import "../styles.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hi om</h1>
-    </div>
+    <Base>
+      <h1 className="text-white">Hello om</h1>
+    </Base>
   );
 }
